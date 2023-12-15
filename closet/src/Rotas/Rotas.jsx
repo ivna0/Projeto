1287a7b23a8131199c7eccc47c5b975b58027c71
Lara Ivina <129535600+ivna0@users.noreply.github.com>
2023-12-15 14:25:41 -0300

    Add files via upload

--- closet/src/Rotas/Rotas.jsx
+++ closet/src/Rotas/Rotas.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cadastro from "../components/Cadastro/Cadastro";
+import Login from "../components/Login/Login";
+import CdtProduto from "../components/CdtProduto/CdtProduto";
 import Filtro from "../components/Filtro/Filtro";
 import Home from "../components/Home/Home";
 import Curtidas from "../components/Curtidas/Curtidas";
 import Detalhes from "../components/DetalhesPdt/Detalhes";
+import Vendas from "../components/Vendas/RelatorioV";
+import Perfil from "../components/Perfil/Perfil"
+// import NavAdm from "../components/NavBarADM/NavAdm";
 
 function Rotas() {
   return (
@@ -15,7 +21,12 @@ function Rotas() {
           <Route path="/filtro" element={<Filtro />} />
           <Route path="/curtidas" element={<Curtidas />} />
           <Route path="/detalhes" element={<Detalhes/>} />
-
+          <Route path="/vendas" element={<Vendas/>} />
+          <Route path="/cdtproduto" element={<CdtProduto/>} /> 
+          <Route path="/cadastro" element={<Cadastro/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/perfil" element={<Perfil/>} />
+          {/* <Route path="/navadm" element={<NavAdm/>} /> */}
         </Routes>
       </BrowserRouter>
     </div>
